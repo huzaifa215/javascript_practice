b92f9df203de381c95857d98b79da2464b92b55d
Hafiz M Huzaifa Khalid <66369515+huzaifa215@users.noreply.github.com>
2021-07-07 22:26:37 +0500

    Submit Form

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 console.log("______________Java Script________________");
 
 const age = 30;
@@ -90,7 +89,6 @@ const studentsData=[
     },
 }
 ];
-
 // use Filter
 // const studentRollNumebr=prompt("Enter the Student Roll Number");
 // const result=studentsData.filter(function(data){
@@ -100,16 +98,58 @@ const studentsData=[
 // const Json=JSON.stringify(result);
 // console.log(Json);
 
-
 // for(let i=0;i<studentsData.length;i++){
 //     if(studentRollNumebr == studentsData[i].id){
 //         console.log(studentsData[i].data);
 //     }
 // }  
 
+// const list=document.querySelector('.items');
+// //list.children[1].innerText="helo";
+
+// const btn= document.querySelector('.btn');
+// // click,mouseover
+// btn.addEventListener('mouseover',(e)=>{
+//     btn.style.background="red";
+//     document.querySelector('#my-form').style.background="blue";
+//    // document.querySelector('body').style.background="#333";
+//    document.querySelector('body').classList.add('bg-dark');// adding the class fromm the css file
+//     list.children[0].innerText="Huzaifa";
+//     list.children[1].innerText="Khuram";
+//     list.children[2].innerText="Awais";
+//     e.preventDefault();// to end it 
+    
+// });
 
 
+// data from 
 
+// get the ids of the form that you have need
+const form=document.querySelector('#my-form');
+const nameInput=document.querySelector('#name');
+const emailInput=document.querySelector('#email');
+const userlist=document.querySelector('#users');
+const msg=document.querySelector('.msg');
+
+form.addEventListener('submit',(e)=>onSubmit(e));
+function onSubmit(e){
+    e.preventDefault();
+    if(nameInput.value === '' || emailInput.value ===''){
+        msg.classList.add('error');
+        msg.innerHTML="Please Enter the data ";
+
+        setTimeout(()=> msg.remove,3000);
+    }
+    else{
+        const li= document.createElement('li');
+        li.appendChild(document.createTextNode(nameInput.value+" : "+emailInput.value ));
+        userlist.appendChild(li);
+
+        nameInput.value="";
+        emailInput.value="";
+
+    }
+}
 
 
 
